f860e36ee0677a5e2a4a9aa041ebd0a66b92fad8
Zack Dai <45176371+zdrel8ed@users.noreply.github.com>
2019-04-20 14:11:06 -0400

    Update pageNER.py
    from home page, scan one layer to find "contact" page. Generate json file.

--- pageNER.py
+++ pageNER.py
@@ -1,60 +1,77 @@
 import requests,re,json
 from bs4 import BeautifulSoup,Comment
-import pandas as pd 
 
 import spacy
 import en_core_web_sm
 nlp = en_core_web_sm.load()
 
+def site_ner(url):
+  if not url.endswith('/'):
+    url += '/'
+  ner = {'EMAIL':[],'PHONE':[],'ADDRESS':[],'POSTALCODE':[],'LOC':[],'ORG':[],'GPE':[],'PERSON':[],'EVENT':[]}
+  level = 0
+  def page_ner(url0):
+      print(url0)
+      page = requests.get(url0)
+      if page.status_code == 200:
+          soup = BeautifulSoup(page.content, 'html5lib')
+          comm = soup.findAll(text=lambda text:isinstance(text, Comment))
+          [cm.extract() for cm in comm]
+          alltags = soup.findAll(text=True)
+          visable_tags = [t for t in alltags if t.parent.name not in ['style','nav', 'script','script', 'head', 'title', 'meta','nav','link','footer','base','applet','iframe','embed','nodembed','object','param','source','[document]']]
+          
+          tags = soup.select("a['href']")
+          links = [tag['href'] for tag in tags if tag['href'].startswith(url) or tag['href'].startswith(url.replace('http','https')) or not tag['href'].startswith('http')]
+          if links:
+            links = [link if link.startswith(url) else url+re.findall(r'\W*(\w.*)',link)[0] for link in links if re.findall(r'\W*(\w.*)',link)]
+            links = list(set(links))
+            contactlinks = [cl for cl in links if 'contact' in cl]
+          
+          for tag in visable_tags:
+              tag = re.sub(r'[\n\s\r\t/]+',' ', tag)
+              if re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', tag):
+                  phone = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', tag)
+                  for ph in phone:
+                      ner['PHONE'].append(ph)
+              else:
+                  phone = []
+              if re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", tag):
+                  email = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", tag)
+                  for em in email:
+                      ner['EMAIL'].append(em)
+              else:
+                  email = []         
+              if re.findall(r'\b[0-9]{5}(?:-[0-9]{4})?\b', tag) or re.findall(r'\b([A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d)\b', tag):
+                  postalcode = re.findall(r'\b[0-9]{5}(?:-[0-9]{4})?\b', tag) + re.findall(r'\b([A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d)\b', tag)
+                  for pc in postalcode:
+                      ner['POSTALCODE'].append(pc)
+              else:
+                  postalcode = []
 
-def page_ner(url):
-    page = requests.get(url)
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.content, 'html5lib')
-        comm = soup.findAll(text=lambda text:isinstance(text, Comment))
-        [cm.extract() for cm in comm]
-        alltags = soup.findAll(text=True)
-        visable_tags = [t for t in alltags if t.parent.name not in ['style','nav', 'script','script', 'head', 'title', 'meta','nav','link','footer','base','applet','iframe','embed','nodembed','object','param','source','[document]']]
-
-        ner = []
-        for tag in visable_tags:
-            tag = re.sub(r'[\n\s\r\t/]+',' ', tag)
-            if re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', tag):
-                phone = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', tag)
-                for ph in phone:
-                    ner.append({'phone':ph})
-            else:
-                phone = []
-            if re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text):
-                email = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text)
-                for em in email:
-                    ner.append({'email':em})
-            else:
-                email = []         
-            if re.findall(r'^[0-9]{5}(?:-[0-9]{4})?$', text) or re.findall(r'^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$', text):
-                postalcode = re.findall(r'^[0-9]{5}(?:-[0-9]{4})?$', text) + re.findall(r'^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$', text)
-                for pc in postalcode:
-                    ner.append({'postalCode':pc})
-            else:
-                postalcode = []
-
-            doc = nlp(str(tag))
-            if len(doc.ents)>0:
+              doc = nlp(str(tag))
+              if len(doc.ents)>0:
+                for x in doc.ents:
+                  for lb in ['LOC','ORG','GPE','PERSON','EVENT']:
+                    if x.label_ == lb:
+                      ner[lb].append(x.text)
+                labels = [x.label_ for x in doc.ents]
+                texts = [x.text for x in doc.ents]
                 if tag not in ' '.join(phone+email+postalcode):
-                    label = ','.join([x.label_ for x in doc.ents])
-                    ner.append({label:tag})
-            dd = pd.read_json(json.dumps(ner))
-            dd.drop_duplicates(inplace=True)
-            dd['link'] = url
-        return dd
-    else:
-        print(url, ' connection failed!')
+                  if len(labels) >1:
+                    if labels[0] in ['CARDINAL','DATE'] and labels[1] in ['LOC','ORG','GPE','PERSON']:
+                      ner['ADDRESS'].append(' '.join(texts))
+          return contactlinks
+      else:
+          print(url0, ' connection failed!')
+          
+  contactlinks = page_ner(url)
+  for cl in contactlinks:
+    page_ner(cl)
+    
+  for key in ner.keys():
+    ner[key] = list(set(ner[key]))
+  return ner
 
-
-url = 'http://www.rel8ed.to/contact-us/'
-info = page_ner(url)
+url = 'https://brocku.ca'
+info = site_ner(url)
 print(info)
-
-
-
-
