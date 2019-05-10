a19f40bb2c4678dc9dfd3307fa9db94bddd706a6
Zack Dai <45176371+zdrel8ed@users.noreply.github.com>
2019-05-10 15:31:14 -0400

    Create getCity.py

---
+++ getCity.py
@@ -0,0 +1,25 @@
+with open('city-in-canada.csv','rb') as f:
+    cities = f.read().decode('utf8').split('\n')[1:]
+    cities = [c for c in cities if ',' in c]
+
+def get_city(url):
+    if not url.startswith('http://'):
+        url = 'http://'+url
+    page = str(requests.get(url).content)
+    print(page)
+    with open('tt.txt','w') as t:
+      t.write(page)
+    page = re.sub(r'\W',' ',page)
+    page = re.sub(r'\s+',' ',page)
+    print(page)
+    cand = []
+    for city in cities:
+        cc = city.split(',')
+        if city.replace(',', ' ') in page:
+            cand.append(city)
+    print(cand)
+    if cand:
+        return cand[-1]
+        return ''
+      
+get_city('www.brocku.ca')
