93fbe3968ff69ff605942cf2c9415898f41a8d94
Zack Dai <45176371+zdrel8ed@users.noreply.github.com>
2019-05-11 17:39:21 -0400

    Update getCity.py

--- getCity.py
+++ getCity.py
@@ -1,14 +1,10 @@
 import requests, re
-import pandas as pd
 
-data = pd.read_csv('https://raw.githubusercontent.com/zdrel8ed/pageNER/master/city-in-canada.csv')
-data.loc[:,['city','province']]
-
-dataa = data.loc[:,['city','province']].to_csv(index=0, header=0)
-datab = data.loc[:,['city','abbrev']].to_csv(index=0, header=0)
-data0 = dataa+datab
-cities = data0.replace('\r','').split('\n')[1:]
-cities = [c for c in cities if ',' in c]
+page = requests.get('https://raw.githubusercontent.com/zdrel8ed/pageNER/master/city-in-canada.csv').content.decode('utf8','ignore')
+rows = page.split('\n')
+parta = [','.join(r.split(',')[:-1]) for r in rows]
+partb = [','.join([r.split(',')[0],r.split(',')[-1]]) for r in rows]
+cities = (parta + partb)[1:-1]
 
 def get_city(url):
     if not url.startswith('http://'):
