0a596f5c934e7ed1642361f46280b054e36a2768
Zack Dai <45176371+zdrel8ed@users.noreply.github.com>
2019-05-11 17:14:04 -0400

    Update getCity.py

--- getCity.py
+++ getCity.py
@@ -1,12 +1,14 @@
 import requests, re
+import pandas as pd
 
-with open('city-in-canada.csv','rb') as f:
-    cities = f.read().decode('utf8')..replace('\r','').split('\n')[1:]
-    cities = [c for c in cities if ',' in c]
+data = pd.read_csv('https://raw.githubusercontent.com/zdrel8ed/pageNER/master/city-in-canada.csv')
+data.loc[:,['city','province']]
 
-parta = [','.join(c.split(',')[:-1]) for c in cities]
-partb = [','.join([c.split(',')[0],c.split(',')[-1]]) for c in cities]
-cities = parta + partb
+dataa = data.loc[:,['city','province']].to_csv(index=0, header=0)
+datab = data.loc[:,['city','abbrev']].to_csv(index=0, header=0)
+data0 = dataa+datab
+cities = data0.replace('\r','').split('\n')[1:]
+cities = [c for c in cities if ',' in c]
 
 def get_city(url):
     if not url.startswith('http://'):
@@ -14,19 +16,15 @@ def get_city(url):
     header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
     html = requests.get(url, headers=header)
     page = str(html.content)
-    print(page)
     with open('tt.txt','w') as t:
       t.write(page)
     page = re.sub(r'\W',' ',page)
     page = re.sub(r'\s+',' ',page).lower()
-    print(page)
     cand = []
     for city in cities:
         cc = city.split(',')
         if city.replace(',', ' ').lower() in page:
-            print('yes')
             cand.append(city)
-    print(cand)
     if cand:
         return cand[-1], html.status_code
     else:
